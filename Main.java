69c06488751e38e8298b48724ab6346412a9e034
peppermintpatty5 <peppermintpatty5.scratch@gmail.com>
2019-11-09 16:23:34 -0500

    sound snippet

--- Main.java
+++ Main.java
@@ -1,6 +1,11 @@
+import java.io.BufferedInputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 
 /**
  * Main
@@ -18,5 +23,19 @@ public class Main {
 
         window.add(new Component(tanks));
         window.setVisible(true);
+    }
+
+    private static synchronized void playSound(String url) {
+        new Thread(() -> {
+            try {
+                Clip clip = AudioSystem.getClip();
+                AudioInputStream inputStream = AudioSystem.getAudioInputStream(new BufferedInputStream(
+                        Object.class.getResourceAsStream("/minesweeper/resources/sounds/" + url)));
+                clip.open(inputStream);
+                clip.start();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
     }
 }
