82062e1defeb4e940aed5f9cfb45f2b41bca38ce
shanemichaelarcaro <sma237@njit.edu>
2019-11-09 15:02:10 -0500

    Good job boys

--- GenAlg.java
+++ GenAlg.java
@@ -30,14 +30,14 @@ public class GenAlg {
 		this.keep = keep;
 	}
 	
-	public Tank[] process(Tank[] currentGeneration) { 
+	public List<Tank> process(List<Tank> currentGeneration) { 
 		
 		// Find the highest and lowest fitness present within a current generation
-		double lowestFitness = currentGeneration[0].getBrain().getFitness();
-		double highestFitness = currentGeneration[0].getBrain().getFitness();
+		double lowestFitness = currentGeneration.get(0).getBrain().getFitness();
+		double highestFitness = currentGeneration.get(0).getBrain().getFitness();
 		
-		for(int i = 1; i < currentGeneration.length; i++) {
-			double currentFitness = currentGeneration[i].getBrain().getFitness();
+		for(int i = 1; i < currentGeneration.size(); i++) {
+			double currentFitness = currentGeneration.get(0).getBrain().getFitness();
 			if(lowestFitness > currentFitness)
 				lowestFitness = currentFitness;
 			if(currentFitness > highestFitness)
@@ -45,8 +45,8 @@ public class GenAlg {
 		}
 		
 		// Normalize the fitness of every Tank
-		for(int i = 0; i < currentGeneration.length; i++)
-			currentGeneration[i].getBrain().setFitness((int)((currentGeneration[i].getBrain().getFitness() - lowestFitness) / (highestFitness - lowestFitness) * geneMultiplier) + 1);
+		for(int i = 0; i < currentGeneration.size(); i++)
+			currentGeneration.get(0).getBrain().setFitness((int)((currentGeneration.get(0).getBrain().getFitness() - lowestFitness) / (highestFitness - lowestFitness) * geneMultiplier) + 1);
 		
 		// Create a proportional gene pool of the current generation
 		List<Tank> genePool = new ArrayList<Tank>();
@@ -58,6 +58,31 @@ public class GenAlg {
 		}
 		
 		return null;
+	}
+	
+	public Tank crossover(Tank father, Tank mother) {
+		double[] fWeight = father.getBrain().getWeights();
+		double[] mWeight = mother.getBrain().getWeights();
+		
+		int length = fWeight.length;
+		int split = (int)(Math.random() * length);
+		
+		double[] fatherWeights = new double[split];
+		double[] motherWeights = new double[length - split];
+		
+		for(int i = 0; i < fatherWeights.length; i++) 
+			fatherWeights[i] = fWeight[i];
+		for(int i = fatherWeights.length - 1; i < mWeight.length; i++)
+			motherWeights[i] = mWeight[i];
+		
+		Tank child = new Tank(0, 0, 0);
+		Arrays.
+		child.getBrain().sendWeights(weights);
+		
+	}
+	
+	public Tank mutate(Tank tank) {
+		
 	}
 }
 
--- Tank.java
+++ Tank.java
@@ -1,13 +1,24 @@
-/**
- * Tank
+/*
+ * Name: Shane Arcaro
+ * File: Tank.java
+ * Date: Nov 9, 2019
+ * Description: 
  */
-public class Tank {
-    int x, y;
-    double theta;
 
-    public Tank(int x, int y, double theta) {
-        this.x = x;
-        this.y = y;
-        this.theta = theta;
-    }
+public class Tank {
+	private int x, y;
+	private double theta;
+	private Brain brain = new Brain(3, 2, 1);
+
+	public Tank(int x, int y, double theta) {
+		this.x = x;
+		this.y = y;
+		this.theta = theta;
+	}
+	
+	public Brain getBrain() {
+		return brain;
+	}
 }
+
+
