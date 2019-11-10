b83432172ee72701870f3312815dc24aba3edbb8
shanemichaelarcaro <sma237@njit.edu>
2019-11-09 20:25:40 -0500

    KILL ME

--- AnimatedObj.java
+++ AnimatedObj.java
@@ -1,5 +1,6 @@
 import java.awt.Dimension;
 import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 
 /**
@@ -17,8 +18,12 @@ public interface AnimatedObj {
 		var i = getAnimationState();
 		var d = getSpriteDimension();
 		var subimg = getSpriteSheet().getSubimage(d.width * i, 0, d.width, d.height);
-		var rotimg = ImageRotation.rotateImage(subimg, theta);
+		
+		AffineTransform backup = g2.getTransform();
+	    AffineTransform rotation = AffineTransform.getRotateInstance(-theta, subimg.getWidth() / 2, subimg.getHeight() / 2);
+	    g2.setTransform(rotation);
+	    g2.drawImage(subimg, x, y, null);
+	    g2.setTransform(backup);
 
-		g2.drawImage(rotimg, x, y, null);
 	}
 }
